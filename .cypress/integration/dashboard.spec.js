30a77b99650acf8124c84b8a1ebb3a09a069722d
Joshua Li <joshuali925@gmail.com>
2021-05-18 15:01:39 -0700

    Remove 'only' in cypress test

--- .cypress/integration/dashboard.spec.js
+++ .cypress/integration/dashboard.spec.js
@@ -62,7 +62,7 @@ describe('Testing dashboard table', () => {
     cy.contains('383.05').should('exist');
   });
 
-  it.only('Opens latency trend popover', () => {
+  it('Opens latency trend popover', () => {
     setTimeFilter(true);
     cy.get('.euiButtonIcon[aria-label="Open popover"]').first().click();
     cy.get('text.ytitle[data-unformatted="Hourly latency (ms)"]').should('exist');
